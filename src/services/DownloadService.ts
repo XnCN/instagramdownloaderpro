6a1fd26c4a5c8a4e8d705aa9ab795700a68ab926
Stiven Trizky Katuuk <pemudakoding@gmail.com>
2024-08-27 00:34:59 +0800

    reverse engineering response

--- src/services/DownloadService.ts
+++ src/services/DownloadService.ts
@@ -13,8 +13,18 @@ class DownloadService implements IDownloadService {
       v: "v2",
     });
     if (data.status != "ok") throw Error("Api error");
+    
+    //reversing Code
+    let reversedHtml = '';
+
+    eval(data.data.replace('eval', 'reversedHtml=')); // execute code, before execute change eval to variable assign
+
+    const patterns = /document\.getElementById\("download-result"\)\.innerHTML\s*=\s*"([\s\S]+?)";/i;
+    const match = reversedHtml.match(patterns); // get the html
+    const html = match[1].replaceAll('\\', ''); // remove backslashes from the html
+
     //encrytpe data
-    const dom = parse(data.data);
+    const dom = parse(html);
     const downloadItems: DownloadResponse[] = [];
     const items = dom.querySelectorAll(".download-box li");
     items.forEach((i) => {
